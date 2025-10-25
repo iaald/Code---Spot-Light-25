af99829f954ca429aab11367734fcf5169c98f3c
Nanamaki <553291650@qq.com>
2025-10-25 13:39:16 +0900

    桌宠Animation Controller

---
+++ Assets/Scripts/DesktopPetAnimation/AnimationChangeControl.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class AnimationChangeControl : MonoBehaviour
+{
+    private Animator animator;
+    private string currentAnimation=("");
+    [SerializeField] private int testparameter=0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(testparameter!=0)
+        {
+            if (testparameter == 1)
+            {
+                AnimationChangeTo("Happy1");
+            }
+            if (testparameter == 2)
+            {
+                AnimationChangeTo("Angry",1.5f);
+            }
+        }
+    }
+    void AnimationChangeTo(string animationName,float crossFadeTime=0.15f)
+    {
+        if(currentAnimation != animationName) 
+        {
+            currentAnimation = animationName;
+            animator.CrossFade(animationName, crossFadeTime);
+        }
+    }
+    // Update is called once per frame
+    
+}
