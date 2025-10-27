eb5b162d2fcc92575e15d845fd914423f1502279
iaald <the_iaald@163.com>
2025-10-27 21:50:12 +0800

    桌宠对话带选项

--- Assets/Scripts/DialogueBox/PetUI/WindowPetDialogueBox.cs
+++ Assets/Scripts/DialogueBox/PetUI/WindowPetDialogueBox.cs
@@ -1,6 +1,7 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
-using Unity.IO.LowLevel.Unsafe;
+using QFramework;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,12 +10,14 @@ public class WindowPetDialogueBox : MonoBehaviour
 {
     public TextMeshProUGUI textMeshProUGUI;
     public float textSpeed = 0.05f; // 每个字符出现的间隔时间
-
+    public string currenContent;
+    private RectTransform rectTransform;
     private Coroutine typingCoroutine;
 
     // 调用此函数开始打印文字
     public void ShowText(string fullText)
     {
+        currenContent = fullText;
         // 如果上一次的打印还在进行，先停止
         if (typingCoroutine != null)
         {
@@ -24,6 +27,7 @@ public class WindowPetDialogueBox : MonoBehaviour
         typingCoroutine = StartCoroutine(TypeText(fullText));
     }
 
+    // 打印协程
     private IEnumerator TypeText(string textToType)
     {
         textMeshProUGUI.text = "";  // 清空当前文本
@@ -37,7 +41,7 @@ public class WindowPetDialogueBox : MonoBehaviour
         typingCoroutine = null; // 打印完成
     }
 
-    // 可选：跳过动画，立即显示完整文字
+    // 直接显示完整文字
     public void ShowFullTextInstant(string fullText)
     {
         if (typingCoroutine != null)
@@ -48,50 +52,76 @@ public class WindowPetDialogueBox : MonoBehaviour
 
         textMeshProUGUI.text = fullText;
     }
-#if UNITY_EDITOR
-    [SerializeField][TextArea] private string stringTotest;
-    [ContextMenu("TestPrint")]
-    public void TestPrint()
+
+    // 跳过打印过程
+    public void SkipTyping()
     {
-        ShowText(stringTotest);
-    }
-#endif
-
-    [HideInInspector] public int dialogueCounter = 0;
-    public TempDialogues tempDialogues;
-
-    private RectTransform rectTransform;
-
-    public void NextDialogue()
-    {
-        if (dialogueCounter == tempDialogues.content.Length - 1)
-        {
-            return;
-        }
+        if (currenContent.IsNullOrEmpty()) return;
         if (typingCoroutine != null)
         {
-            ShowFullTextInstant(tempDialogues.content[dialogueCounter]);
+            ShowFullTextInstant(currenContent);
             LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
             return;
         }
-        ShowText(tempDialogues.content[++dialogueCounter]);
-    }
-    public void LoadAndStartDialogue(string d_name)
-    {
-        dialogueCounter = 0;
-        LoadDialogue(d_name);
-        ShowText(tempDialogues.content[0]);
-    }
-    public void LoadDialogue(string d_name)
-    {
-        var t = Resources.Load<TempDialogues>(d_name);
-        this.tempDialogues = t;
-    }
-    void Start()
-    {
-        rectTransform = GetComponent<RectTransform>();
-        LoadAndStartDialogue("DialoguesData_Test");
     }
 
+    public class Option
+    {
+        public int index;
+        public string caption;
+        public GameObject buttonObj;
+        public Option(int index, string caption = "")
+        {
+            this.index = index;
+            this.caption = caption;
+        }
+        override public string ToString()
+        {
+            return "" + index;
+        }
+    }
 
+    public GameObject OptionButtonTemplate;
+    List<Option> optsButtons = new();
+    
+    // pass options=null to clear all option buttons
+    public void SetOptions(List<Option> options)
+    {
+        if (optsButtons.Count != 0)
+        {
+            foreach (var item in optsButtons)
+            {
+                if (item.buttonObj != null)
+                {
+                    Destroy(item.buttonObj);
+                }
+            }
+            optsButtons.Clear();
+        }
+        if (options == null) return;
+        foreach (var option in options)
+        {
+            var temp = Instantiate(OptionButtonTemplate, transform);
+            TextMeshProUGUI TMP = temp.GetComponentInChildren<TextMeshProUGUI>();
+            Button button = temp.GetComponentInChildren<Button>();
+            TMP.text = option.caption;
+            button.onClick.AddListener(() =>
+            {
+                // 在这里注册一个事件发送
+                Debug.Log($"Here you should send an event - Option IDX {option}");
+            });
+            option.buttonObj = temp;
+            optsButtons.Add(option);
+        }
+    }
+
+    // Example:
+    // ShowText("Some test text"); // show text (printer)
+    // yield return new WaitForSeconds(2);
+
+    // Set options (switch to a new option group)
+    // List<Option> options = new() { new(1, "A"), new(2, "B"), new(3, "C") };
+    // SetOptions(options);
+
+    // Invoke(nameof(tsan), 5f);
 }
