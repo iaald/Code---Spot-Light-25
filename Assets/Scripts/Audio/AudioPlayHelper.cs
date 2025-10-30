996247a2f3e6c5c03fc9c5407967b894de2ffc74
Nanamaki <553291650@qq.com>
2025-10-30 18:36:03 +0900

    老师弹窗

--- Assets/Scripts/Audio/AudioPlayHelper.cs
+++ Assets/Scripts/Audio/AudioPlayHelper.cs
@@ -7,18 +7,22 @@ public class AudioPlayHelper : MonoBehaviour
     public AudioClip audioClip;
     public void PH()
     {
+        if (AudioMng.Instance == null || audioClip == null) return;
         AudioMng.Instance.PlayHigherOneshot(audioClip);
     }
     public void PL()
     {
+        if (AudioMng.Instance == null || audioClip == null) return;
         AudioMng.Instance.PlayLowerOneshot(audioClip);
     }
     public void PC()
     {
+        if (AudioMng.Instance == null || audioClip == null) return;
         AudioMng.Instance.PlayCurrentOneshot(audioClip);
     }
     public void PlayClickSound()
     {
+        if (AudioMng.Instance == null || audioClip == null) return;
         AudioMng.Instance.PlaySound("click");
     }
 }
